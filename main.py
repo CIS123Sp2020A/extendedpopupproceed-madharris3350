d16f9431860b0c5448e7caaeb229b9af12752f53
madharris3350 <60237202+madharris3350@users.noreply.github.com>
2020-04-09 12:41:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-#Your name goes here
+#Madison Harris
+#this is my extended pop up code
+from tkinter import *
+import tkinter.messagebox as box
 
-#Your algorithm should go here OR you should use comments throughout
+#The first step after imports is create a window
+window = Tk()
+#This shows up at the top of the frame
+window.title("Message Box Example")
 
-#Your code goes here
-import tkinter
-import tkinter.messagebox as box 
 
 
-# Create the main window
-self.main_window = tkinter.Tk()
+def dialog():
+    var = box.askyesno("Message Box", "Proceed?")
+    if var == 1:
+        box.showinfo("Yes Box", "Proceeding...")
+    else:
+        box.showwarning("No Box", "Cancelling...")
+
+#creating a button
+btn = Button(window, text="Click", command=dialog)
+# I have to pack the button
+
+btn.pack(padx=150, pady=50)
+#start the action and keep it going
+window.mainloop()
